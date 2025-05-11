e88c08aebbbe27c31cfbfff81b67b4b3bac854fd
Nicolas Désilles <nicolas@ndlabs.fr>
2025-05-11 19:16:06 +0200

    Finished Exercise 6.6

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -45,7 +45,7 @@ export const createAnecdote = (content) => {
 }
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
+  //console.log('state now: ', state)
   console.log('action', action)
 
   switch (action.type) {
