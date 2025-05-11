6237a526be23cdbf2d50e57e4caae6b7f4e7ca36
Nicolas Désilles <nicolas@ndlabs.fr>
2025-05-11 19:57:44 +0200

    Finished Exercise 6.9

--- redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,14 @@ import { useSelector, useDispatch } from 'react-redux'
 import { addVoteTo } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => {
+        if (state.filter === '') {
+            return state.anecdotes
+        }
+        return state.anecdotes.filter(
+          a => (a.content.toLowerCase().includes(state.filter) || a.content.includes(state.filter))
+        )
+    })
     const dispatch = useDispatch()
 
     const vote = (id) => {
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -44,7 +44,7 @@ export const createAnecdote = (content) => {
   }
 }
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
   //console.log('state now: ', state)
   console.log('action', action)
 
@@ -68,4 +68,4 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-export default reducer
+export default anecdoteReducer
